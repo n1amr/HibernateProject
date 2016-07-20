494f58744591a9f32511bd31a1adeb8e33fe48db
Amr Alaa <n1amr1@gmail.com>
2016-07-20 14:10:17 +0200

    add views

--- src/script.sql
+++ src/script.sql
@@ -1,13 +1,19 @@
 -- USE TEST;
 
-DELETE FROM amr_order_items;
-DELETE FROM amr_place_items_menu;
-DELETE FROM amr_orders;
-DELETE FROM amr_places;
-DELETE FROM amr_user;
+-- DELETE FROM amr_order_items;
+-- DELETE FROM amr_place_items_menu;
+-- DELETE FROM amr_orders;
+-- DELETE FROM amr_places;
+-- DELETE FROM amr_user;
+
+DROP TABLE amr_order_items;
+DROP TABLE amr_place_items_menu;
+DROP TABLE amr_orders;
+DROP TABLE amr_places;
+DROP TABLE amr_user;
 
 CREATE TABLE amr_user (
-  ID       NUMBER(32) PRIMARY KEY NOT NULL ,
+  ID       NUMBER(32) PRIMARY KEY NOT NULL,
   Username VARCHAR2(255) UNIQUE,
   Pass     VARCHAR2(255),
   Name     VARCHAR2(255),
@@ -15,31 +21,32 @@ CREATE TABLE amr_user (
 );
 
 CREATE TABLE amr_places (
-  ID      NUMBER(32) PRIMARY KEY NOT NULL ,
+  ID      NUMBER(32) PRIMARY KEY NOT NULL,
   Name    VARCHAR2(255),
   PhoneNo VARCHAR2(50)
 );
 
 CREATE TABLE amr_place_items_menu (
   ID      NUMBER(32) PRIMARY KEY,
-  PlaceID NUMBER(32) REFERENCES amr_places,
+  PlaceID NUMBER(32) REFERENCES amr_places NOT NULL,
   Name    VARCHAR2(255),
   "Desc"  CLOB,
-  Price   NUMBER(5, 2)
+  Price   NUMBER(5, 2),
+  CONSTRAINT "NON_NEGATIVE_PRICE" CHECK (Price >= 0) ENABLE
 );
 
 CREATE TABLE amr_orders (
-  ID           NUMBER(32) PRIMARY KEY NOT NULL ,
-  Owner_userID NUMBER(32) REFERENCES amr_user,
+  ID           NUMBER(32) PRIMARY KEY           NOT NULL,
+  Owner_userID NUMBER(32) REFERENCES amr_user   NOT NULL,
   Status       VARCHAR2(50),
-  PlaceID      NUMBER(32) REFERENCES amr_places,
+  PlaceID      NUMBER(32) REFERENCES amr_places NOT NULL,
   "Date"       DATE
 );
 
 CREATE TABLE amr_order_items (
-  ID                NUMBER(32) PRIMARY KEY NOT NULL ,
-  OrderID           NUMBER(32) REFERENCES amr_orders,
-  Place_Item_MenuID NUMBER(32) REFERENCES amr_place_items_menu,
-  UserID            NUMBER(32) REFERENCES amr_user,
+  ID                NUMBER(32) PRIMARY KEY                     NOT NULL,
+  OrderID           NUMBER(32) REFERENCES amr_orders           NOT NULL,
+  Place_Item_MenuID NUMBER(32) REFERENCES amr_place_items_menu NOT NULL,
+  UserID            NUMBER(32) REFERENCES amr_user             NOT NULL,
   Count             NUMBER(10, 0) DEFAULT (1)
 );
---
+++ src/views.sql
@@ -0,0 +1,25 @@
+-- Order view
+SELECT
+  AMR_ORDERS.ID     AS "Order ID",
+  AMR_USER.NAME     AS "Owner Name",
+  AMR_ORDERS.STATUS AS "Status",
+  AMR_PLACES.NAME   AS "Plcae Name",
+  AMR_ORDERS."Date" AS "Date"
+FROM AMR_ORDERS
+  JOIN AMR_PLACES ON AMR_ORDERS.PLACEID = AMR_PLACES.ID
+  JOIN AMR_USER ON AMR_ORDERS.OWNER_USERID = AMR_USER.ID;
+
+-- Order item view
+SELECT
+  AMR_ORDERS.ID                                               AS "Order ID",
+  AMR_PLACES.NAME                                             AS "Place Name",
+  AMR_USER.USERNAME                                           AS "Username",
+  AMR_ORDER_ITEMS.COUNT                                       AS "Count",
+  ('$' || AMR_PLACE_ITEMS_MENU.PRICE)                         AS "Price",
+  ('$' || AMR_ORDER_ITEMS.COUNT * AMR_PLACE_ITEMS_MENU.PRICE) AS "Total"
+FROM AMR_ORDER_ITEMS
+  JOIN AMR_USER ON AMR_ORDER_ITEMS.USERID = AMR_USER.ID
+  JOIN AMR_PLACE_ITEMS_MENU ON AMR_ORDER_ITEMS.PLACE_ITEM_MENUID = AMR_PLACE_ITEMS_MENU.ID
+  JOIN AMR_ORDERS ON AMR_ORDER_ITEMS.ORDERID = AMR_ORDERS.ID
+  JOIN AMR_PLACES ON AMR_ORDERS.PLACEID = AMR_PLACES.ID;
+
